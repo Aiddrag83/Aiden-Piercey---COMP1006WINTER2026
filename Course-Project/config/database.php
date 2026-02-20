7687797f9ce9d0b755a76c702a9fe1495c9fe66c
Aiddrag83 <aidenpiercey83@gmail.com>
2026-02-19 19:01:51 -0500

    Course Project

---
+++ Course-Project/config/database.php
@@ -0,0 +1,16 @@
+<?php
+// Database connection file
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "resume_builder";
+
+// Create a connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Checking said connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
