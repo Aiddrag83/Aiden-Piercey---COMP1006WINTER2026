b67d0c3577d2d524ef9d5e69a27710989527dc24
Aiden Piercey <aidenpiercey83@gmail.com>
2026-03-06 13:35:31 -0500

    Midterm

--- Aiden Piercey - Midterm_COMP1006_Winter2026/EXAM QUESTION/config/database.php
+++ Aiden Piercey - Midterm_COMP1006_Winter2026/EXAM QUESTION/config/database.php
@@ -4,7 +4,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "resume_builder";
+$database = "book_manager";
 
 // Create a connection
 $conn = new mysqli($host, $username, $password, $database);
