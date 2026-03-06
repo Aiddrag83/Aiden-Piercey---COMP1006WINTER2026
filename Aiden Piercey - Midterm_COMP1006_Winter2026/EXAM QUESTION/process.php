c5530cadf4be2b99f33f95b57718a4f5aeacae50
Aiden Piercey <aidenpiercey83@gmail.com>
2026-03-06 12:19:05 -0500

    EXAM QUESTION

---
+++ Aiden Piercey - Midterm_COMP1006_Winter2026/EXAM QUESTION/process.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Using the provided HTML form:
+- Accept user input
+- Sanitize and validate the form data on the server
+- If valid, store the review in the database
+- If invalid, display an error message and do not insert the record
+*/ 
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    die('Invalid request');
+}
+
+$firstName = trim(filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_SPECIAL_CHARS));
+$lastName  = trim(filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_SPECIAL_CHARS));
+$email     = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$phone     = trim(filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_SPECIAL_CHARS));
