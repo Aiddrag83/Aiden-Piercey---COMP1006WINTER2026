5ff862aa4584ff3cd8e7c77eaae0091c17ce1d0d
Aiden Piercey <aidenpiercey83@gmail.com>
2026-04-09 13:52:05 -0400

    Added Captcha, Register.php

---
+++ Course-Project/public/config.php
@@ -0,0 +1,8 @@
+<?php
+// Database configuration
+
+define('DB_HOST', 'localhost');    // Usually localhost on XAMPP
+define('DB_USER', 'root');         // Default XAMPP MySQL username
+define('DB_PASS', '');             // Default XAMPP MySQL password is empty
+define('DB_NAME', 'course_project'); // Replace with your actual database name
+?>
